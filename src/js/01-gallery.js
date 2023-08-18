bd08be512c55b599068565f8811206563761c2da
starodubbb <e.molodid2011@gmail.com>
2023-08-18 20:06:03 +0300

    add task 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.innerHTML = makeGalleryItemsMarkup(galleryItems);
+
+let gallery = createGallery();
+
+console.log(gallery);
+
+function createGallery() {
+  return new SimpleLightbox('.gallery__item a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+function makeGalleryItemsMarkup(items) {
+  return items.reduce((acc, item) => {
+    return (acc += `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+    `);
+  }, '');
+}
